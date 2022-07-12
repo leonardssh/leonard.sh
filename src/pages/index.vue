6628a4027239929cea2ebff675cdac74931209a6
Narcis B <leonardssh22@gmail.com>
2022-07-12 18:45:59 +0200

    chore: new font

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,9 @@
 <template>
-	<div class="min-h-screen bg-black font-karla">
+	<div class="min-h-screen bg-black">
 		<div class="absolute top-[50vh] left-[50vw] -translate-x-1/2 -translate-y-1/2">
-			<h1 class="text-4xl text-white">Be right back.</h1>
+			<h1 class="text-6xl font-extrabold text-white uppercase font-monument-extended">
+				Be right back.
+			</h1>
 		</div>
 	</div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,14 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: 'class',
-	content: ['./src/**/*.{js,jsx,ts,tsx,vue}'],
+	content: ['./src/**/*.{ts,vue}'],
 	theme: {
 		extend: {
 			fontFamily: {
 				inter: ['Inter', ...defaultTheme.fontFamily.sans],
 				karla: ['Karla', ...defaultTheme.fontFamily.sans],
-				inconsolata: ['Inconsolata', ...defaultTheme.fontFamily.mono]
+				inconsolata: ['Inconsolata', ...defaultTheme.fontFamily.mono],
+				'monument-extended': ['Monument Extended', ...defaultTheme.fontFamily.sans]
 			}
 		}
 	},
