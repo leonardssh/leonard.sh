47a489dc3ca62bcaecf7962e6ed548ef648d7bee
Narcis B <leonardssh22@gmail.com>
2022-07-12 18:45:15 +0200

    fix: vite ssr hmr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 import { defineNuxtConfig, type NuxtConfig } from 'nuxt';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 const config: NuxtConfig = {
 	typescript: {
 		shim: false,
@@ -8,6 +10,14 @@ const config: NuxtConfig = {
 
 	srcDir: 'src',
 	target: 'static',
+
+	/*
+    Disabling server-side rendering on development mode because
+    Vite module currently doesn't work when SSR is enabled. This
+    might cause some issues and/or hydration errors but will be
+    effective enough to help you develop easier.
+  */
+	ssr: !isDev,
 
 	css: ['~/assets/css/fonts.css', '~/assets/css/tailwind.css'],
 	build: {
