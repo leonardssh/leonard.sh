a6c9df42e728b2d25f8a3b247ef4a4527a1286c1
Narcis B <leonardssh22@gmail.com>
2022-07-13 23:07:14 +0200

    refactor: increase prettier printWidth

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,7 @@
 <template>
 	<div class="min-h-screen bg-black">
 		<div class="absolute top-[50vh] left-[50vw] -translate-x-1/2 -translate-y-1/2">
-			<h1 class="text-6xl font-extrabold text-white uppercase font-monument-extended">
-				Be right back.
-			</h1>
+			<h1 class="text-6xl font-extrabold text-white uppercase font-monument-extended">Be right back.</h1>
 		</div>
 	</div>
 </template>
