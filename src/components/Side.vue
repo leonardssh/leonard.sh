cbe6ad8370c9a149ba82fa734ffd4e6f2403aa52
Narcis B <leonardssh22@gmail.com>
2022-07-16 23:49:43 +0200

    style: reduce padding between sides

--- src/components/Side.vue
+++ src/components/Side.vue
@@ -9,8 +9,8 @@ const { layout } = defineProps<Props>();
 <template>
 	<div
 		:class="{
-			'container mx-auto lg:w-[50%] w-[100%] px-14 2xl:px-10 pb-20 lg:pb-10': layout === 'left',
-			'bg-[url(/images/right-bg.jpg)] bg-cover bg-[#101926] text-white px-14 lg:px-18 lg:w-[40%] w-[100%] pb-10': layout === 'right'
+			'container mx-auto lg:w-[50%] w-[100%] px-8 pb-20 lg:pb-10': layout === 'left',
+			'bg-[url(/images/right-bg.jpg)] bg-cover bg-[#101926] text-white px-8 lg:w-[40%] w-[100%] pb-10': layout === 'right'
 		}"
 	>
 		<slot />
