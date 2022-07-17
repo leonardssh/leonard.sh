aafadc9c5cc1aa8e547961638af3634ca59ae1c4
Narcis B <leonardssh22@gmail.com>
2022-07-17 23:19:06 +0200

    chore: remove linkedin from socials

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -16,16 +16,12 @@ const socials: Social[] = [
 	{
 		name: 'instagram',
 		url: 'https://www.instagram.com/leonardssh22/'
-	},
-	{
-		name: 'linkedin',
-		url: 'https://www.linkedin.com/in/leonardssh/'
 	}
 ];
 </script>
 
 <template>
-	<div class="flex items-end justify-center mt-5 space-x-2 text-3xl cursor-pointer lg:text-2xl lg:m-0 text-slate-500">
+	<div class="flex items-end justify-center mt-5 space-x-3 text-3xl cursor-pointer lg:text-2xl lg:m-0 text-slate-500">
 		<template v-for="(social, key) in socials" :key="key">
 			<nuxt-icon class="transition-all duration-200 hover:text-slate-700" :name="social.name" @click="$openUrlInNewTab(social.url)" />
 		</template>
