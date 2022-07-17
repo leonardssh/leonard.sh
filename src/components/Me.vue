924d9423eef79bdcf10b88cb7d2666353607d844
Narcis B <leonardssh22@gmail.com>
2022-07-18 00:40:29 +0200

    style: center me component layout

--- src/components/Me.vue
+++ src/components/Me.vue
@@ -16,10 +16,10 @@ const { elementRef: meRef } = useTooltip({
 				<DiscordStatus />
 			</div>
 		</div>
-		<div v-motion-slide-bottom class="flex flex-col items-center justify-between w-[calc(100%-5rem)] ml-6 lg:flex-row">
+		<div v-motion-slide-bottom class="flex flex-col items-center justify-between w-[calc(100%-5rem)] m-0 lg:ml-6 xl:ml-6 2xl:ml-6 lg:flex-row">
 			<div class="flex flex-col items-start justify-center">
 				<h1 class="m-auto mt-6 text-3xl font-bold font-inter slide-top lg:m-0">Narcis B.</h1>
-				<h2 class="m-auto text-lg font-medium font-inter text-slate-500 slide-top lg:m-0">Full Stack Web Developer</h2>
+				<h2 class="m-auto text-lg font-medium text-center font-inter text-slate-500 slide-top lg:m-0">Full Stack Web Developer</h2>
 			</div>
 			<Socials v-motion-slide-right :delay="950" />
 		</div>
