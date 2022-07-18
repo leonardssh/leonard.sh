a9bb2ef800c6cc0acdd087ccab2626a6d8187384
Narcis B <leonardssh22@gmail.com>
2022-07-18 13:35:13 +0200

    style: use margin for skills

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="flex flex-col justify-between pt-8 cursor-default sm:flex-row lg:flex-col xl:flex-col 2xl:flex-row">
+	<div class="flex flex-col justify-between mt-5 cursor-default sm:flex-row lg:flex-col xl:flex-col 2xl:flex-row">
 		<SkillsDevelopment />
 		<SkillsPersonal />
 		<SkillsInterests />
