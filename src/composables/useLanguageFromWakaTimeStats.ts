c2bc05f1fc5f54e86d23d5317f353129e734087f
Narcis B <leonardssh22@gmail.com>
2022-07-18 20:56:18 +0200

    fix: wakatime language stats composable

--- src/composables/useLanguageFromWakaTimeStats.ts
+++ src/composables/useLanguageFromWakaTimeStats.ts
@@ -1,5 +1,5 @@
 import { WakaTimeLanguage } from '~/types/wakatime';
 
 export function useLanguageFromWakaTimeStats(languages: WakaTimeLanguage[], language: string): WakaTimeLanguage | null {
-	return languages.find((lang) => lang.name === language);
+	return languages && languages.find((lang) => lang.name === language);
 }
