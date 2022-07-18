00c5e9d252c504bab175cc0999ebd37eebdcdace
Narcis B <leonardssh22@gmail.com>
2022-07-18 21:16:46 +0200

    fix: trying to fix env

--- src/server/api/projects/index.get.ts
+++ src/server/api/projects/index.get.ts
@@ -14,9 +14,9 @@ const getRepository = async ([owner, name]: [string, string], token: string): Pr
 const cache = new InMemoryCache<IProject[]>();
 
 export default defineEventHandler(async () => {
-	// if (cache.get('projects') && !cache.expired('projects')) {
-	// 	return cache.get('projects');
-	// }
+	if (cache.get('projects') && !cache.expired('projects')) {
+		return cache.get('projects');
+	}
 
 	const { app } = useRuntimeConfig();
 
@@ -28,22 +28,20 @@ export default defineEventHandler(async () => {
 		})
 	);
 
-	return response;
+	const projects = response
+		.map(({ name, full_name, description, forks_count, stargazers_count, html_url, language }) => ({
+			name,
+			full_name,
+			description,
+			forks_count,
+			forks_url: `${html_url}/network/members`,
+			stargazers_count,
+			stargazers_url: `${html_url}/stargazers`,
+			html_url,
+			language
+		}))
+		.sort((a, b) => b.stargazers_count - a.stargazers_count);
 
-	// const projects = response
-	// 	.map(({ name, full_name, description, forks_count, stargazers_count, html_url, language }) => ({
-	// 		name,
-	// 		full_name,
-	// 		description,
-	// 		forks_count,
-	// 		forks_url: `${html_url}/network/members`,
-	// 		stargazers_count,
-	// 		stargazers_url: `${html_url}/stargazers`,
-	// 		html_url,
-	// 		language
-	// 	}))
-	// 	.sort((a, b) => b.stargazers_count - a.stargazers_count);
-
-	// cache.set('projects', projects);
-	// return projects;
+	cache.set('projects', projects);
+	return projects;
 });
