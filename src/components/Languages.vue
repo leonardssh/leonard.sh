69bd22a627adc00fda01f11f5198edb3afe10dc5
Narcis B <leonardssh22@gmail.com>
2022-07-18 15:34:32 +0200

    style: styling languages

--- src/components/Languages.vue
+++ src/components/Languages.vue
@@ -20,7 +20,7 @@ const languages = [
 		<h4 class="mb-2 text-base font-medium text-slate-400">Languages</h4>
 		<template v-for="({ lang, level }, key) of languages" :key="key">
 			<div class="inline-block">
-				<span class="font-semibold">{{ lang }}</span>
+				<span class="text-sm font-medium">{{ lang }}</span>
 				<span class="ml-1 text-sm">({{ level }}){{ key < languages.length - 1 ? ',&nbsp;' : '' }}</span>
 			</div>
 		</template>
