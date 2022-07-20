80578fba21d1d19e3b16448ad976d3a4ec5521d8
Narcis B <leonardssh22@gmail.com>
2022-07-20 19:48:27 +0200

    chore: check if dist folder exists

--- scripts/get-resume.js
+++ scripts/get-resume.js
@@ -2,14 +2,19 @@ const axios = require('axios');
 const fs = require('fs');
 
 const RESUME_URL = 'https://github.com/leonardssh/resume/raw/main/resume.pdf';
+const OUTPUT_DIR = './dist';
 
 async function getResumePDF() {
+	if (!fs.existsSync(OUTPUT_DIR)) {
+		return;
+	}
+
 	axios({
 		method: 'get',
 		url: RESUME_URL,
 		responseType: 'stream'
 	}).then((response) => {
-		response.data.pipe(fs.createWriteStream('./dist/resume.pdf'));
+		response.data.pipe(fs.createWriteStream(`${OUTPUT_DIR}/resume.pdf`));
 	});
 }
 
