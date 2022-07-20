c51d8e829b34419ad53692d7d9ecb9446b071623
Narcis B <leonardssh22@gmail.com>
2022-07-20 15:33:50 +0200

    feat: add resume

---
+++ src/components/PDF.vue
@@ -0,0 +1,7 @@
+<template>
+	<div class="flex items-center justify-center mt-5">
+		<a href="https://leonard.sh/resume.pdf" class="text-sm font-inter font-normal border-b pb-[0.5px] border-dotted border-black" target="_blank">
+			PDF Version
+		</a>
+	</div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -27,6 +27,7 @@ watch(lanyard, () => {
 		<Experience />
 		<Education />
 		<Certifications />
+		<PDF />
 	</Side>
 
 	<Side v-motion-slide-right :delay="250" layout="right">
